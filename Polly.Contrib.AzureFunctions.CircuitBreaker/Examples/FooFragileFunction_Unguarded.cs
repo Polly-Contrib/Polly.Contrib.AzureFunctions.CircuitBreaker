072d2655ebf74084839e8b5316acde6e34dc3376
reisenberger <software@reisenberger.net>
2019-07-24 08:30:07 +0100

    Add example function not guarded by circuit-breaker

---
+++ Polly.Contrib.AzureFunctions.CircuitBreaker/Examples/FooFragileFunction_Unguarded.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Polly.Contrib.AzureFunctions.CircuitBreaker.Examples
+{
+    public class FooFragileFunction_Unguarded
+    {
+        // Used by this demonstration code to generate random failures of the simulated work.
+        private static readonly Random Rand = new Random();
+
+        public FooFragileFunction_Unguarded()
+        {
+        }
+
+        [FunctionName("FooFragileFunction_Unguarded")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequestMessage req,
+            ILogger log,
+            [OrchestrationClient]IDurableOrchestrationClient orchestrationClient
+            )
+        {
+            return await OriginalFunctionMethod(req, log);
+        }
+        
+        private static async Task<IActionResult> OriginalFunctionMethod(HttpRequestMessage req, ILogger log)
+        {
+            // Do something fragile!
+            if (Rand.Next(2) == 0)
+            {
+                /*await Task.Delay(TimeSpan.FromSeconds(1));*/
+                throw new Exception("Something fragile went wrong.");
+            }
+
+            // Do some work and return some result.
+            await Task.CompletedTask;
+
+            var helloWorld = "Hello world: from inside the function.";
+            log.LogInformation(helloWorld);
+
+            return new OkObjectResult(helloWorld);
+        }
+    }
+}
